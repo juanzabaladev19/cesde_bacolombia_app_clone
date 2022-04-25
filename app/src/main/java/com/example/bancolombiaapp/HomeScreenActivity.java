57146cd7e039f5c18d2520de36dab80cc313cea5
zabaladev <juanzabaladev@gmail.com>
2022-04-25 14:28:51 -0500

    added home screen

---
+++ app/src/main/java/com/example/bancolombiaapp/HomeScreenActivity.java
@@ -0,0 +1,31 @@
+package com.example.bancolombiaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bancolombiaapp.databinding.ActivityHomeScreenBinding;
+import com.example.bancolombiaapp.databinding.BottomBarComponentBinding;
+
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    private ActivityHomeScreenBinding homeScreenBinding;
+    private BottomBarComponentBinding bottomBarComponentBinding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        homeScreenBinding = ActivityHomeScreenBinding.inflate(getLayoutInflater());
+        bottomBarComponentBinding = BottomBarComponentBinding.inflate(getLayoutInflater());
+        View view = homeScreenBinding.getRoot();
+        setContentView(view);
+        homeScreenBinding.bottomBarComponent.iconHomeLayout.setBackgroundColor(getResources().getColor(R.color.bc_yellow));
+        homeScreenBinding.btnSignIn.setOnClickListener((v) -> {
+            Intent intent = new Intent(HomeScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+    }
+}
