7455362fae898ae9d89ff477454c683d42bfceae
Maria Camila <camilarxn023@gmail.com>
2022-05-22 17:14:00 -0500

    Interface confirmacion transaccion

---
+++ app/src/main/java/com/example/bancolombiaapp/TransactionCompleted.java
@@ -0,0 +1,14 @@
+package com.example.bancolombiaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransactionCompleted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_completed);
+    }
+}
